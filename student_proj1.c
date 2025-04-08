d6f586ae59f3bf9152a6664ceeb3d79921118086
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-04-08 19:14:00 +0530

    slight optimised source file

--- student_proj1.c
+++ student_proj1.c
@@ -66,7 +66,7 @@ int main(){
                                     break;
                                  default :
                                      printf("INVALID !");
-                                    break;
+                                    
                              
                                 }
                                  printf("\n-------------------------");//students info separtor
